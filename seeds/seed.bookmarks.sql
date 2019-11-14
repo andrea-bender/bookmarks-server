aed69d9cd79381a3ca9db1c2390010351f725194
Andrea Bender <andreab.1726@gmail.com>
2019-11-14 14:22:38 -0700

    completed GET and tests for bookmarks server

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+('Google', 'www.google.com',
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?', 5),
+  ('yahoo', 'www.yahoo.com',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.', 3),
+  ('github', 'www.github.com',
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.', 4),
+  ('youtube', 'www.youtube.com',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, consequuntur. Cum quo ea vero, fugiat dolor labore harum aut reprehenderit totam dolores hic quaerat, est, quia similique! Aspernatur, quis nihil?', 4),
+  ('amazon', 'www.amazon.com',
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet soluta fugiat itaque recusandae rerum sed nobis. Excepturi voluptas nisi, labore officia, nobis repellat rem ab tempora, laboriosam odio reiciendis placeat?', 4);
---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,38 @@
+/* eslint-disable indent */
+/* eslint-disable strict */
+function makeBookmarksArray() {
+    return [
+        {
+            id: 1,
+            title: 'First test post!',
+            url: 'www.test1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+            rating: 5
+        },
+        {
+            id: 2,
+            title: 'Second test post!',
+            url: 'www.test2',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+            rating: 4
+        },
+        {
+            id: 3,
+            title: 'third test post!',
+            url: 'www.test3',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+            rating: 3
+        },
+        {
+            id: 4,
+            title: 'fourth test post!',
+            url: 'www.test4',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+            rating: 2
+        },
+    ];
+}
+
+module.exports = {
+    makeBookmarksArray,
+}
